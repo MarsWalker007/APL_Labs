d2abc121d2133eb98952742856437bc87e1fd6a8
Alexey <plaxinrus@yandex.ru>
2021-04-13 14:40:06 +0500

    2laba

--- lab2.c
+++ lab2.c
@@ -2,16 +2,16 @@
 #include <math.h>
 int main()
 {
-  double x;
-  printf("Enter x { 8.98 <= x <= 9.26 } -> ");
-  scanf("%lf", &x);
-  if (x > 8.98 && x < 9.26)
-  {
-    double y = acos(2.0 * sin(x));
-    double z = sqrt(cos(y * y));
-    printf("y(x) = %lf\nz(y) = %lf\n", y, z);
-  }
-  else
-    printf("x value is incorrect!\n");
-  return 0;
+    double x;
+    printf("Enter x { 8.98 <= x <= 9.26 } -> ");
+    scanf("%lf", &x);
+    if (x >= 8.98 && x <= 9.26)
+    {
+        double y = acos(2.0 * sin(x));
+        double z = sqrt(cos(y * y));
+        printf("y(x) = %lf\nz(y) = %lf\n", y, z);
+    }
+    else
+        printf("x value is incorrect!\n");
+    return 0;
 }
