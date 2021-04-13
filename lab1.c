8f5e5bb1cc9661254048c5b8ea6660a5cf820924
Alexey <plaxinrus@yandex.ru>
2021-04-13 13:15:09 +0500

    1

--- lab1.c
+++ lab1.c
@@ -2,29 +2,36 @@
 #include <math.h>
 int main ()
 {
-  float x, y, z, max, min1, min2;
-  printf("Enter x -> ");
-  scanf("%f", &x);
-  printf("Enter y -> ");
-  scanf("%f", &y);
-  printf("Enter z -> ");
-  scanf("%f", &z);
-  if(x>y && x>z)
-  {
-    max=x;
-    min1=1;
-    min2=2;
+    float x,y,z,max,min,min1;
+    printf("Enter x -> ");
+    scanf("%f", &x);
+    printf("Enter y -> ");
+    scanf("%f", &y);
+    printf("Enter z -> ");
+    scanf("%f", &z);
+    if (x>y && x>z)
+    {
+        max=x;
+        min=y;
+        min1=z;
+    }
+    if (y>x && y>z)
+    {
+        max=y;
+        min=x;
+        min1=z;
+    }
+    if (z>x && z>y)
+    {
+        max=z;
+        min=x;
+        min1=y;
+    }
+    {
+        if (max>min+min1)
+            printf("%f",max);
+        else
+            printf("%f\n",(min+min1)-max);
+    }
+    return 0;
   }
-  if(y>x && y>z)
-  {
-    max=y;
-    min1=1;
-    min2=2;
-  }
-  if(z>x && z>y)
-  {
-    max=z;
-    min1=1;
-    min2=2;
-  }
-}
