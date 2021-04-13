d4e7c310f2c1175172283d1f8995dc9a9a98dd7f
Alexey <plaxinrus@yandex.ru>
2021-04-13 14:35:32 +0500

    2laba

---
+++ lab2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  double x;
+  printf("Enter x { 8.98 <= x <= 9.26 } -> ");
+  scanf("%lf", &x);
+  if (x > 8.98 && x < 9.26)
+  {
+    double y = acos(2.0 * sin(x));
+    double z = sqrt(cos(y * y));
+    printf("y(x) = %lf\nz(y) = %lf\n", y, z);
+  }
+  else
+    printf("x value is incorrect!\n");
+  return 0;
+}
